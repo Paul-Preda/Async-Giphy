3103636240e20c8b24aa3cf8fda37978fc7c3fa2
Paul Preda <ppd1512@gmail.com>
2023-05-02 16:01:27 +0100

    initial commit to get an image by using my key and a different query

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 require('dotenv').config();
 
+
+let endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=cat&limit=25&offset=0&rating=g&lang=en`
+console.log(endpoint)
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+
+async function getImage(query){
+    let newUrl = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+    let res = await fetch(newUrl)
+    let dat = await res.json();
+    let data = dat["data"]
+    let x = data[0]
+    let url = x["url"]
+    console.log(url)
+
+    console.log("This is the new Url with the queary of dog " + Url)
+}
+
+getImage("dog")
